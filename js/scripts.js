74d15f368bc848b965f8890c1b1eda714ca5cb75
Jerrod Styrk <jstyrk@citadel.edu>
2020-06-08 10:57:57 -0700

    Create javascript functions to display output
    Co-authored-by: Mook Dang <mookdang24@gmail.com>
    Co-authored-by: Sead Downs <spdowns@protonmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,16 @@
+$(document).ready(function() {
+  $("form#favorite").submit(function(event) {
+    event.preventDefault();
+  
+    let color = $("input#color").val();
+    let band = $("input#band").val();
+  
+    let favThings = [band, color]
 
+    console.log(favThings)
+
+    $("#output").text(favThings);
+
+  });
+});
 
